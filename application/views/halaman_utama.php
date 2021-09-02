1348e26f4dc7d33d0e2fffa5350ca08345874bce
asep-krwn <asepkurni2w2n@gmail.com>
2021-09-03 00:05:28 +0700

    Perubahan Judul

--- application/views/halaman_utama.php
+++ application/views/halaman_utama.php
@@ -6,7 +6,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
   <section class="py-sm-5 py-3 text-center container">
     <div class="row py-sm-4 px-3 animate__animated animate__fadeInUp animate__faster" id="main">
       <div class="col-xl-6 col-lg-8 col-md-8 my-sm-4 my-5 mx-auto">
-        <p class="fw-bold h2">Konversi Gambarmu ke PDF</p>
+        <p class="fw-bold h2">Konversi vidio to jpg</p>
         <p class="fw-bold h2">Dengan Mudah & Aman</p>
         <p class="fw-bold lh-lg keterangan">Setelah dikonversi, file kamu <mark>langsung dihapus!</mark></p>
         <div class="my-sm-5 my-4 mx-md-0 animate__animated animate__fadeInUp" id="kotak_upload">
